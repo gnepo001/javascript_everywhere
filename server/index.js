79e313951f7aaf19499d648e813dc030ff25bacf
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-04-12 21:18:01 -0700

    rewrite

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 import express from "express";
 
 const app = express();
+
+const port = process.env.PORT || 4000;
+
 app.get("/", (req, res) => res.send("hello world"));
-app.listen(4000, () => console.log("listening on port 4000!"));
+
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
