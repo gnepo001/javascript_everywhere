951a0246fa1d463a8afbdeb6e0e9ed3b65ed66e3
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-04-12 21:42:35 -0700

    graphql

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 import express from "express";
-
-const app = express();
+import { ApolloServer, gql } from "apollo-server-express";
 
 const port = process.env.PORT || 4000;
 
-app.get("/", (req, res) => res.send("hello world"));
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
+const resolvers = {
+  Query: {
+    hello: () => "Hello World",
+  },
+};
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+await server.start();
+server.applyMiddleware({ app, path: "/api" });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
 );
