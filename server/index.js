3f4901e9bbcfeb4af559e94ff97b0cb7863826f4
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-06-26 21:49:41 -0700

    back to learning

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import db from "./db.js";
+
 const port = process.env.PORT || 4000;
+
+const DB_HOST = process.env.DB_HOST;
 
 let notes = [
   { id: "1", content: "This is a note", author: "Adam Scott" },
@@ -47,6 +55,8 @@ const resolvers = {
 };
 
 const app = express();
+
+db.connect(DB_HOST);
 
 const server = new ApolloServer({ typeDefs, resolvers });
 await server.start();
