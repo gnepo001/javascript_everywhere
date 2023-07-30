ead8edf295bb03e8faea84b194c92335106be523
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-07-30 14:27:14 -0700

    JWT

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 import db from "./db.js";
 import models from "./models/index.js";
@@ -16,13 +17,32 @@ const app = express();
 
 db.connect(DB_HOST);
 
+// get the user info from a JWT
+const getUser = (token) => {
+  if (token) {
+    try {
+      // return the user information from the token
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      // if there's a problem with the token, throw an error
+      throw new Error("Session invalid");
+    }
+  }
+};
+
 //apollo server setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    //add db models to the context
-    return { models };
+  context: ({ req }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization;
+    // try to retrieve a user with the token
+    const user = getUser(token);
+    // for now, let's log the user to the console:
+    console.log(user);
+    // add the db models and the user to the context
+    return { models, user };
   },
 });
 
