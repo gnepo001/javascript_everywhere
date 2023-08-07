e3ef895d68f953b2d857c8264bf4c8a85372e7a3
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-08-06 23:01:55 -0700

    components added

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+
 const Home = () => {
   return (
     <div>
-      <h1>Notedly</h1>
-      <p>This is the Home page</p>
+      <Header />
+      <Navigation />
+      <p>This is the home page</p>
     </div>
   );
 };
