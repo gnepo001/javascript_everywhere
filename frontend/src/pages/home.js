9fe972ce4bef1a773a256819e6dffa212aabb601
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-08-09 21:30:19 -0700

    added components

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
-import ReactMarkdown from "react-markdown";
+
+import NoteFeed from "../components/NoteFeed";
 
 //our graphql query, stored as a variable
 const GET_NOTES = gql`
@@ -33,22 +34,7 @@ const Home = () => {
   //if there is an error fetching the data, display an error message
   if (error) return <p>Error!</p>;
 
-  return (
-    <div>
-      {data.noteFeed.notes.map((note) => (
-        <article>
-          <img
-            src={note.author.avatar}
-            alt={`${note.author.username} avatar`}
-            height="50px"
-          />
-          {note.author.username} {note.createdAt} {note.favoriteCount}
-          {""}
-          <ReactMarkdown source={note.content} />
-        </article>
-      ))}
-    </div>
-  );
+  return <NoteFeed notes={data.noteFeed.notes} />;
 };
 
 export default Home;
