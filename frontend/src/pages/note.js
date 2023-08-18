d978acb56a32ca694d927420d213f2900809a8f8
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-08-17 21:44:10 -0700

    adding id with gql

--- frontend/src/pages/note.js
+++ frontend/src/pages/note.js
@@ -1,14 +1,34 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+
+import Note from "../components/Note.js";
+
+const GET_NOTE = gql`
+  query note($id: ID!) {
+    note(id: $id) {
+      id
+      createdAt
+      content
+      favoriteCount
+      author {
+        username
+        id
+        avatar
+      }
+    }
+  }
+`;
 
 const NotePage = (props) => {
   const { id } = useParams();
 
-  return (
-    <div>
-      <p>ID: {id}</p>
-    </div>
-  );
+  const { loading, error, data } = useQuery(GET_NOTE, { variables: { id } });
+
+  if (loading) return <p>LOADING...</p>;
+  if (error) return <p>Error! Note not Found</p>;
+
+  return <Note note={data.note} />;
 };
 
 export default NotePage;
