3d346d031ac21eae25984a554c3d2e1d7045c070
Gilbert Nepomuceno <gilbertnepomuceno99@gmail.com>
2023-08-17 21:30:26 -0700

    api dynamic added

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -7,6 +7,7 @@ import Layout from "../components/Layout.js";
 import Home from "./home.js";
 import MyNotes from "./mynotes.js";
 import Favorites from "./favorites.js";
+import NotePage from "./note.js";
 
 const Pages = () => {
   return (
@@ -17,6 +18,7 @@ const Pages = () => {
             <Route exact path="/" element={<Home />} />
             <Route path="/mynotes" element={<MyNotes />} />
             <Route path="/favorites" element={<Favorites />} />
+            <Route path="/note/:id" element={<NotePage />} />
           </Routes>
         </Layout>
       </BrowserRouter>
